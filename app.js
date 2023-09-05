f5044e4245b7261044efa4f9bb8d3b8b2ce12e36
Maharaja <108454691+maha17p@users.noreply.github.com>
2023-09-05 04:01:18 +0000

    check dotenv file

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+const PORT_NUM = 5000;
+
+
+app.listen(PORT_NUM,() => console.log(`App is listening to ${PORT_NUM}`))
